b8a1c0f6a91d4c0f4cc6e4ea41442c1babcd580b
FennekT <Matthias.T1@gmail.com>
2021-02-18 09:43:08 +0100

    Buttons

--- app/src/main/java/com/example/trainingsplan/TrainingsplanActivity.java
+++ app/src/main/java/com/example/trainingsplan/TrainingsplanActivity.java
@@ -11,7 +11,7 @@ public class TrainingsplanActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trainingsplan);
-        ScrollView scrollView = findViewById(R.id.scroll);
+        ScrollView penisView = findViewById(R.id.scroll);
 
     }
 }
