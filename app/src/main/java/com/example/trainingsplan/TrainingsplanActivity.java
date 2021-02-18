8aeed26c0c2375eec760e6b24a56fae31b5018ec
Storm_Dev <marcaurel.korn@gmail.com>
2021-02-18 09:11:57 +0100

    f

---
+++ app/src/main/java/com/example/trainingsplan/TrainingsplanActivity.java
@@ -0,0 +1,14 @@
+package com.example.trainingsplan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TrainingsplanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trainingsplan);
+    }
+}
