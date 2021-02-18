efb3e640856d12ea169cf3da75f54545f0ec8be1
Storm_Dev <marcaurel.korn@gmail.com>
2021-02-18 09:41:22 +0100

    f

--- app/src/main/java/com/example/trainingsplan/MainActivity.java
+++ app/src/main/java/com/example/trainingsplan/MainActivity.java
@@ -33,6 +33,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if (v.getId() == R.id.Trainingsplan) {
             Intent intent = new Intent(this, TrainingsplanActivity.class);
             startActivity(intent);
+
         }
     }
 }
