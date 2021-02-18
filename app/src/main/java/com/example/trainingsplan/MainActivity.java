fd8396bfb23810a49bed5eaaf53595e2f07a1fee
FennekT <Matthias.T1@gmail.com>
2021-02-18 09:03:49 +0100

    FirstCommit

--- app/src/main/java/com/example/trainingsplan/MainActivity.java
+++ app/src/main/java/com/example/trainingsplan/MainActivity.java
@@ -11,29 +11,12 @@ import android.widget.TextView;
 
 import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity implements TextToSpeech.OnInitListener, View.OnClickListener {
-
-    private TextToSpeech tts;
+public class MainActivity extends AppCompatActivity  {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tts = new TextToSpeech(this, this);
 
-    }
-
-
-    @Override
-    public void onInit(int status) {
-        tts.setLanguage(Locale.JAPANESE);
-        Button button = findViewById(R.id.buttonSampleText);
-        button.setOnClickListener(this);
-    }
-
-    @Override
-    public void onClick(View v) {
-        TextView textView = findViewById(R.id.editTextTextPersonName);
-        tts.speak(textView.getText().toString(), TextToSpeech.QUEUE_FLUSH, Bundle.EMPTY, null);
     }
 }
