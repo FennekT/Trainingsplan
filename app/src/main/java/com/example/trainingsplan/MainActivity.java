d8a6d07919619553aca1773ccd04e532c672c805
FennekT <Matthias.T1@gmail.com>
2021-02-18 10:15:43 +0100

    CreateTrainingsplanActivity hinzugefügt, + Button erstellt + listener

--- app/src/main/java/com/example/trainingsplan/MainActivity.java
+++ app/src/main/java/com/example/trainingsplan/MainActivity.java
@@ -4,15 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.speech.tts.TextToSpeech;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.PopupMenu;
-import android.widget.PopupWindow;
-import android.widget.TextView;
-
-import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -20,7 +13,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button trainingsplan = findViewById(R.id.Trainingsplan);
+        Button trainingsplan = findViewById(R.id.trainingsplan);
         //Noch nicht implementiert
         //Button kalendar = findViewById(R.id.Kalendar);
         //Button wasserkonsum = findViewById(R.id.Wasserkonsum);
@@ -30,7 +23,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        if (v.getId() == R.id.Trainingsplan) {
+        if (v.getId() == R.id.trainingsplan) {
             Intent intent = new Intent(this, TrainingsplanActivity.class);
             startActivity(intent);
 
--- app/src/main/java/com/example/trainingsplan/TrainingsplanActivity.java
+++ app/src/main/java/com/example/trainingsplan/TrainingsplanActivity.java
@@ -2,14 +2,17 @@ package com.example.trainingsplan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 
 import com.example.trainingsplan.objekte.Trainingsplan;
 
-public class TrainingsplanActivity extends AppCompatActivity {
+public class TrainingsplanActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,12 +25,21 @@ public class TrainingsplanActivity extends AppCompatActivity {
         LinearLayout linearLayout = new LinearLayout(this);
         linearLayout.setOrientation(LinearLayout.VERTICAL);
 
-// Add Buttons
+        // Add Buttons
         Button button = new Button(this);
         button.setText(plan.getName());
         linearLayout.addView(button);
+        ImageButton addButton = findViewById(R.id.addButton);
 
-// Add the LinearLayout element to the ScrollView
+        // Add the LinearLayout element to the ScrollView
         scrollView.addView(linearLayout);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.addButton) {
+            Intent intent = new Intent(this, CreateTrainingsplanActivity.class);
+            startActivity(intent);
+        }
     }
 }
