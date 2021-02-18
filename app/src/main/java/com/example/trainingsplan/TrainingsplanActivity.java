9e297462c482e27988bfa26dcb5f0a3098ebb8fe
Storm_Dev <marcaurel.korn@gmail.com>
2021-02-18 09:18:51 +0100

    f

--- app/src/main/java/com/example/trainingsplan/TrainingsplanActivity.java
+++ app/src/main/java/com/example/trainingsplan/TrainingsplanActivity.java
@@ -3,6 +3,7 @@ package com.example.trainingsplan;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ScrollView;
 
 public class TrainingsplanActivity extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class TrainingsplanActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trainingsplan);
+        ScrollView scrollView = findViewById(R.id.scroll);
+
     }
 }
