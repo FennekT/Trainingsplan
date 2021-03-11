88529157a2d32cee5963c301fb4f083062c2e01e
FennekT <Matthias.T1@gmail.com>
2021-03-11 11:41:01 +0100

    import

--- app/src/main/java/com/example/trainingsplan/TrainingsplanCreationActivity.java
+++ app/src/main/java/com/example/trainingsplan/TrainingsplanCreationActivity.java
@@ -2,6 +2,7 @@ package com.example.trainingsplan;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
